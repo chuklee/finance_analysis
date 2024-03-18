dea9014dc218b004315875524052afc1f87a1cfe
davidcyn974 <david.chane-yock-nam@epita.fr>
2024-03-18 13:37:38 +0100

    still have to understand how insert all files will take no longer time and how to get Company_id (cid). However the overall struct for getting and inserting to stocks should be okay, except for cid .

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -3,50 +3,64 @@ import numpy as np
 import sklearn
 import os
 import timescaledb_model as tsdb
+from datetime import datetime, timezone
 
-db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
-#db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
+#db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
+db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
 
 def store_file(name, website):
-   
     if website.lower() == "boursorama":
-        print("ouHOOO")
-        try:
-            df = pd.read_pickle("/docker/data/boursorama/" + name)  # is this dir ok for you ?
+        """ try:
+            df = pd.read_pickle("../docker/data/boursorama/" + name)  # is this dir ok for you ?
         except:
-            year = name.split()[1].split("-")[0]
-            df = pd.read_pickle("../docker/data/boursorama/" + year + "/" + name)
-        print(df)
-        
-        df_companies= df[['name', 'symbol']]
-        df_companies = df_companies.drop(columns=['symbol'])
-        #df_companies = df_companies.drop_duplicates(subset='name', keep='first')  not used for the moment
-        print(df_companies)
-        db.df_write(df_companies, "companies", commit=True)
+         """   
+        # name = amsterdam 2019-01-01 090502.607291.bz2
+        # compB 2019-12-31 171201.644550.bz2
+        # amsterdam and compB are the market's name
+        market_name = name.split()[0]
+        year = name.split()[1].split("-")[0]
+        # Split the filename to extract the date and time components
+        parts = name.split()
+        date_str = parts[1] 
+        time_str = parts[2].replace("", ":")  # Extract time component and replace Unicode character with colon
+        time_str = time_str.replace(".bz2", "")  # Remove the .bz2 extension from the time component
 
-        ''' 
-        Does not work since the company id is not handled for the moment ...
+        # Combine date and time components
+        timestamp_str = f"{date_str} {time_str}"
 
-        df_stocks = df[['last', 'volume', 'name']]
-
-        df_stocks = df_stocks.rename(columns={'last': 'value'})
-        for index, row in df_stocks.iterrows(): 
-            # Extract company name and symbol from the row
-            company_name = row['name']
-            
-            company_id = db.search_company_id(company_name)
-            
-            # Set the 'cid' column in the current row to the company ID
-            df_stocks.at[index, 'cid'] = company_id
+        # Parse the timestamp string into a datetime object
+        timestamp_dt = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
+        df_stocks = pd.read_pickle("../docker/data/boursorama/" + year + "/" + name)
         print(df_stocks)
-        '''
+
+        df_stocks.loc[:, 'date'] = pd.Timestamp(timestamp_dt)
+        # Todo : remove name and replace by cid
+        df_stocks.loc[:, 'cid'] = 0
+        df_stocks.drop(columns = ['symbol'], inplace = True)
+        df_stocks = df_stocks.rename(columns={'last': 'value'})
+        # print(df_stocks)
+        #db.df_write(df_stocks, "stocks", commit=True)
+        
+
+        '''df_companies=''' # to be finished : look into database it will be faster ig
+        #df_companies = df_companies.drop_duplicates(subset='name', keep='first')  not used for the moment
+        #print(df_companies)
+        #db.df_write(df_companies, "companies", commit=True)
+
     # to be finished
-    """ 
-    #Print the companies table
-    request = db.get_companies()
-    for r in request:
-        print(r)
-     """
+   
 if __name__ == '__main__':
-    store_file("amsterdam 2019-01-01 090502.607291.bz2", "boursorama")
-    print("Done")
+    try:
+        store_file("amsterdam 2019-01-01 090502.607291.bz2", "boursorama")
+
+        files = os.listdir("../docker/data/boursorama/2019")
+        for file in files[:50]:
+            print(file)
+        print("Done")
+    except:
+        print("Directory not found")
+        exit(1)
+
+
+     #2021 2022 2023 Y4A DU PEA PME
+     # de ce que j'ai vu il existe seulement compA compB amsterdam et peapme ... pme que pour 2021 2022 2023
