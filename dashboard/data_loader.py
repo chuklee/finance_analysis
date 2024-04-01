b7000dda6790484f18430ab6bae63838b7b1bab1
Vincent <vincent.saunier@epita.fr>
2024-04-01 11:05:40 +0200

    tentative d'integration du travail du bon vieux dave

--- dashboard/data_loader.py
+++ dashboard/data_loader.py
@@ -1,7 +1,10 @@
 import pandas as pd
-
+import sys
+from ..analyzer.timescaledb_model import db
 
 def load_data():
+    return db.df_query('SELECT * FROM select * from daystocks where cid = 2')
     return pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
 
 dataframe_finances = load_data()
+print(dataframe_finances.head())
